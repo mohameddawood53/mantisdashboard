0108e40b75bf006a9b2014725a53e8b2813383f2
sarahmhd <sarahmhd213199@gmail.com>
2023-11-25 13:41:40 +0200

    first commit

---
+++ src/routes/LoginRoutes.js
@@ -0,0 +1,27 @@
+// project import
+import Loadable from 'components/Loadable';
+import MinimalLayout from 'layout/MinimalLayout';
+import { lazy } from 'react';
+
+// render - login
+const AuthLogin = Loadable(lazy(() => import('pages/authentication/Login')));
+const AuthRegister = Loadable(lazy(() => import('pages/authentication/Register')));
+
+// ==============================|| AUTH ROUTING ||============================== //
+
+const LoginRoutes = {
+  path: '/',
+  element: <MinimalLayout />,
+  children: [
+    {
+      path: 'login',
+      element: <AuthLogin />
+    },
+    {
+      path: 'register',
+      element: <AuthRegister />
+    }
+  ]
+};
+
+export default LoginRoutes;
